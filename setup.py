5fd7127bd9a528999ade6ea96d0229b9ff9aec95
Sermet Pekin <sermet.pekin@gmail.com>
2025-10-21 23:21:24 +0300

    fix torch on toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from build_cffi import create_ffibuilder
-
-# Create the CFFI builder
-ffibuilder = create_ffibuilder()
-
-setup(
-    cffi_modules=["build_cffi.py:ffibuilder"],
-)
